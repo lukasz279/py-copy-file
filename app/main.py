7c480a26d52b8840265f6dbf81fc3c2f6a55cb92
Lukasz <lukasz18052016@wp.pl>
2025-03-17 21:43:49 +0100

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,1 +1,15 @@
-# write your code here
+def copy_file(command: str) -> None:
+    command_parts = command.split()
+    if len(command_parts) != 3 or command_parts[0] != "cp":
+        return
+    _, source_file, destination_file = command_parts
+    if source_file == destination_file:
+        return
+    try:
+        with (open(source_file, 'r') as file_in,
+              open(destination_file, 'w') as file_out):
+            file_out.write(file_in.read())
+    except FileNotFoundError:
+        print(f"Error: {source_file} not found.")
+    except IOError as e:
+        print(f"Error occurred during file copy: {e}")
